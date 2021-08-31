07a39cf489f4ce1d529f668a8bc85a1c6a765828
Martin Dome <martindome96@gmail.com>
2021-08-31 02:28:34 -0300

    Adding files

--- DAL/UsuarioDAL.cs
+++ DAL/UsuarioDAL.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using BE;
+using SERV;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -10,6 +11,9 @@ namespace DAL
 {
     class UsuarioDAL
     {
+        static int mId;
+        static SERV.Seguridad.Cifrado mCifra = new SERV.Seguridad.Cifrado();
+        static SERV.Integridad mIntegridad = new SERV.Integridad();
 
         public static List<UsuarioBE> Listar()
         {
@@ -33,6 +37,13 @@ namespace DAL
             return ListaUsuarios;
 
         }
+        private static int ProximoId()
+        {
+            if (mId == 0)
+                mId = (new DAO()).ObtenerUltimoId("Usuario");
+            mId += 1;
+            return mId;
+        }
 
         private static void ValorizarEntidad(UsuarioBE pUsuario, DataRow pDataRow)
         {
@@ -43,5 +54,55 @@ namespace DAL
             pUsuario.Intentos_Login = int.Parse((pDataRow["Intentos_Login"].ToString()));
         }
 
+        public static UsuarioBE Obtener(int pId)
+        {
+            string mCommandText = "Select u.ID_Usuario, u.Nombre, u.Apellido, u.Nombre_Usuario, u.Contraseña, u.Intentos_Login FROM Usuarios u WHERE u.ID_Usuario = " + pId;
+            DAO mDao = new DAO();
+
+            DataSet mDataSet = mDao.ExecuteDataSet(mCommandText);
+
+            if (mDataSet.Tables.Count > 0 && mDataSet.Tables[0].Rows.Count > 0)
+            {
+                UsuarioBE mUsuario = new UsuarioBE(pId);
+                ValorizarEntidad(mUsuario, mDataSet.Tables[0].Rows[0]);
+                return mUsuario;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public static int Guardar(UsuarioBE pUsuario)
+        {
+            if (pUsuario.ID_Usuario == 0)
+            {
+                pUsuario.ID_Usuario = ProximoId();
+                string DVH = mIntegridad.ObtenerDVH(pUsuario.ID_Usuario.ToString() + pUsuario.Nombre + pUsuario.Apellido + pUsuario.Nombre_Usuario + pUsuario.Contrasenia + pUsuario.Intentos_Login.ToString());
+                string mCommand = "INSERT INTO Usuario(ID_Usuario, Nombre, Apellido, Nombre_Usuario, Contraseña, Intentos_Login, DVH) VALUES (" +pUsuario.ID_Usuario + ", '" + pUsuario.Nombre + "', '" + pUsuario.Apellido + "', '" + pUsuario.Nombre_Usuario + "', '" + pUsuario.Contrasenia + "', " +pUsuario.Intentos_Login +", '" + DVH + "')";
+                DAO mDao = new DAO();
+                return mDao.ExecuteNonQuery(mCommand);
+            }
+            else
+            {
+                string DVH = mIntegridad.ObtenerDVH(pUsuario.ID_Usuario.ToString() + pUsuario.Nombre + pUsuario.Apellido + pUsuario.Nombre_Usuario + pUsuario.Contrasenia + pUsuario.Intentos_Login.ToString());
+                string mCommand = "Update Usuario SET Nombre = '" + pUsuario.Nombre
+                    + "', Apellido = '" + pUsuario.Apellido
+                    + "', Nombre_Usuario = '" + pUsuario.Nombre_Usuario
+                    + "', Contraseña = '" + pUsuario.Contrasenia
+                    + "', Intentos_Login = " + pUsuario.Intentos_Login
+                    + ", DVH = '" + DVH + "', WHERE ID_Usuario =" + pUsuario.ID_Usuario;
+                DAO mDao = new DAO();
+                return mDao.ExecuteNonQuery(mCommand);
+            }
+        }
+
+        public static int Eliminar (UsuarioBE pUsuario)
+        {
+            string mCommandText = "DELETE Usuario WHERE ID_Usuario = " + pUsuario.ID_Usuario;
+            DAO mDao = new DAO();
+            return mDao.ExecuteNonQuery(mCommandText);
+        }
     }
 }
