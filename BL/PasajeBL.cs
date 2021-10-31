ee6c94c2623c09679a758e192c015763ad9b7357
Martin Dome <Martin.Dome@Alumnos.uai.edu.ar>
2021-10-31 20:19:38 -0300

    Adding files

--- BL/PasajeBL.cs
+++ BL/PasajeBL.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using DAL;
+using BE;
 
 namespace BL
 {
     public class PasajeBL
     {
+        public List<PasajeBE> Listar()
+        {
+            return PasajeDAL.Listar();
+        }
+
+        public List<PasajeBE> ListarCliente(int pId)
+        {
+            return PasajeDAL.ListarCliente(pId);
+        }
+
+        public PasajeBE Obtener(int pId)
+        {
+            return PasajeDAL.Obtener(pId);
+        }
+
+        public void Guardar(PasajeBE pasaje)
+        {
+            PasajeDAL.Guardar(pasaje);
+        }
+
+        public void Borrar (PasajeBE pasaje)
+        {
+            PasajeDAL.Borrar(pasaje)
+        } 
     }
 }
